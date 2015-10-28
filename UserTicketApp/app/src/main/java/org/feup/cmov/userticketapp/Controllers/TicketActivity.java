d2c98d98a9a975d12da5d2c68516b6374541c24f
André Silva <andre.ee11304@gmail.com>
2015-10-28 18:33:12 +0000

    Added backarrow actions to ticket activities.

--- UserTicketApp/app/src/main/java/org/feup/cmov/userticketapp/Controllers/TicketActivity.java
+++ UserTicketApp/app/src/main/java/org/feup/cmov/userticketapp/Controllers/TicketActivity.java
@@ -33,6 +33,8 @@ public class TicketActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         mTicket = sharedData.getSelectedTicket();
 
         TextView ticketTitleText = (TextView) findViewById(R.id.ticket_title);
