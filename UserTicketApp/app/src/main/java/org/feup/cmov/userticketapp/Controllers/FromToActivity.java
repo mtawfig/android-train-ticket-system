7901c4ece3bc60323770204788994055f1b51ea0
André Silva <andre.ee11304@gmail.com>
2015-11-07 15:03:44 +0000

    Removed settings menu.

--- UserTicketApp/app/src/main/java/org/feup/cmov/userticketapp/Controllers/FromToActivity.java
+++ UserTicketApp/app/src/main/java/org/feup/cmov/userticketapp/Controllers/FromToActivity.java
@@ -79,7 +79,7 @@ public class FromToActivity extends AppCompatActivity implements MapFragment.Sta
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_from_to, menu);
+        //getMenuInflater().inflate(R.menu.menu_from_to, menu);
         return true;
     }
 
--- UserTicketApp/app/src/main/java/org/feup/cmov/userticketapp/Controllers/MainActivity.java
+++ UserTicketApp/app/src/main/java/org/feup/cmov/userticketapp/Controllers/MainActivity.java
@@ -129,7 +129,7 @@ public class MainActivity extends AppCompatActivity implements MapFragment.Stati
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
